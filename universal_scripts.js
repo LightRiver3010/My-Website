42fbd59ff0bc2cfc2837c7d92527299961c39c62
Jimmy <savarynj@gmail.com>
2026-01-21 13:38:36 -0500

    added random image selection for the headshot

--- universal_scripts.js
+++ universal_scripts.js
@@ -13,3 +13,13 @@ proudBoxes[i].source.addEventListener("mouseleave", function() {
     proudBoxes[i].source.classList.add('dulled');
     })
 }
+
+const images = [
+    "assets/eating my food.png",
+    "assets/headshot with torch.png",
+    "assets/pointing to windmill.png",
+    "assets/senior headshot.png",
+    "assets/nice headshot (in tree).jpg"
+]
+const random = Math.floor(Math.random() * images.length);
+document.getElementById('random_headshot_image').src = images[random];
